1b91f2e6500835d5af61de0edf3ed63939d5b883
Sheikh Sohail <sheikhsohail.contact@gmail.com>
2025-10-23 16:54:10 +0530

    Updated for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ummah/", // repo name (case-sensitive)
+  base: "/",
+ // repo name (case-sensitive)
   build: {
     outDir: "dist",
   },
