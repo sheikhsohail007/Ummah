ea72c75fbda784cedc34a5e8ce3b8f2238c68f55
Sheikh Sohail <sheikhsohail.contact@gmail.com>
2025-10-23 17:29:48 +0530

    Fixed base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ummah/",
+   base: '/',
  // repo name (case-sensitive)
   build: {
     outDir: "dist",
