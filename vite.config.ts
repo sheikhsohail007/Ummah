fa5215d2c44f188240a875afcedeebf49a059a75
Sheikh Sohail <sheikhsohail.contact@gmail.com>
2025-10-23 17:12:20 +0530

    uodate for daomin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Ummah/",
  // repo name (case-sensitive)
   build: {
     outDir: "dist",
